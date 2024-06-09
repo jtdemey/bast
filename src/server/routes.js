cba2fe8c959f4cd09175f1eace6a8f5a965c47be
jtdemey <johntdemey@live.com>
2024-06-09 10:25:13 -0400

    Temporary redirect

--- src/server/routes.js
+++ src/server/routes.js
@@ -27,10 +27,13 @@ const sendHtmlFile = (res, fileName) => {
 
 const routeHtml = (endpoint, fileName = undefined) =>
   router.route(endpoint).get((_, res) => {
+    res.redirect("https://johntorsten.com/bast");
+    /*
     sendHtmlFile(
       res,
       `${fileName ? fileName : endpoint.replace("/", "")}.html`,
     );
+    */
   });
 
 ROUTES.forEach((route) =>
